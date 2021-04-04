8b85b989e67f6eb09c3bead0b8efc88d398059e0
MayconMenezes <ap_maycon@hotmail.com>
2021-04-04 16:28:18 -0300

    test: ensure SignUpController return success201 if valid data is provided

--- src/presentation/controllers/signup/signUpController.spec.ts
+++ src/presentation/controllers/signup/signUpController.spec.ts
@@ -218,6 +218,28 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
+  test('should return success201 if valid data is provided', () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(201)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email@email.com',
+      password: 'valid_password'
+    })
+  })
+
   test('should call AddAccount with correct values', () => {
     const { sut, addAccountStub } = makeSut()
     const addAccountSpy = jest.spyOn(addAccountStub, 'add')
