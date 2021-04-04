c809083a27bf6877a357c97b49bf35aa9121b207
MayconMenezes <ap_maycon@hotmail.com>
2021-04-04 16:27:27 -0300

    feat: ensure SignUpController return success201 if valid data is provided

--- src/presentation/controllers/signup/signUpController.ts
+++ src/presentation/controllers/signup/signUpController.ts
@@ -30,13 +30,13 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name, email, password
       })
 
       return {
         statusCode: 201,
-        body: undefined
+        body: account
       }
     } catch (error) {
       return serverError()
